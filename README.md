1bc4be9d1ab783b78b17c45b21337a3f58d02275
Jorge Lucas <37757146+OTrigo@users.noreply.github.com>
2023-11-20 00:36:16 +0000

    feat: adding  on the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do https://www.tabnews.com.br para o https://curso.dev, o melhor curso de programação que existe.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-const Home = ()=>{
-    return <h1>Eu amo a Bruna, a minha namorada e futura esposa.</h1>
-}
+const Home = () => {
+  return <h1>Eu amo a Bruna, a minha namorada e futura esposa.</h1>;
+};
 
-export default Home
+export default Home;
