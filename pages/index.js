779f225c699b3417a4eb031ff50d2e62bd7a9a8f
Jorge Lucas <37757146+OTrigo@users.noreply.github.com>
2023-12-08 21:29:28 +0000

    feat: adding the ´in construction´ page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
+import styles from "./style.module.css";
+
 const Home = () => {
-  return <h1>Eu amo a Bruna, a minha namorada e futura esposa.</h1>;
+  return (
+    <div className={styles.html}>
+      <div className={styles.home}>
+        <div className={styles.app}>
+          <h1 className={styles.title}>
+            <a href="https://otrigo.github.io/" className={styles.link}>
+              {"{PinkWheat}"}
+            </a>
+          </h1>
+          <h2 className={styles.subtitle}>
+            Em breve, um lugarzinho melhor de internet será acessível por você!
+          </h2>
+          <h3 className={styles.tabnews}>
+            Conheça também o{" "}
+            <span className={styles.tabnewsText}>
+              <a href="https://www.tabnews.com.br/" className={styles.link}>
+                Tabnews
+              </a>
+            </span>
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
