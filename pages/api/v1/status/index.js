5b88e9545fde607cadd33c39338f780d6f2ef405
Jorge Lucas <37757146+OTrigo@users.noreply.github.com>
2023-12-14 19:45:48 +0000

    feat: adding `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response){
-  response.status(200).json({mensagem: "mensagem"})
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 AS SUM;")
+  console.log(result.rows)
+  response.status(200).json({ mensagem: "mensagem" });
 }
 
-export default status
+export default status;
