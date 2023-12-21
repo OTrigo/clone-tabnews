5d407ce7347d0e421026a4401d8d6b7f7c7d11e0
Jorge Lucas <37757146+OTrigo@users.noreply.github.com>
2023-12-21 23:47:02 +0000

    feat: `updated_at` and `dependencies` return on the /status - Jorge Lucas version

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 AS SUM;")
-  console.log(result.rows)
-  response.status(200).json({ mensagem: "mensagem" });
+  const updatedAt = new Date().toISOString();
+  const databaseVersion = await database
+    .query(`SHOW server_version`)
+    .then((value) => {
+      return value.rows[0].server_version;
+    });
+
+  const maxConnections = await database
+    .query(`SHOW max_connections`)
+    .then((value) => {
+      return value.rows[0].max_connections;
+    });
+
+  const openedConnections = await database
+    .query(`SELECT count(*) FROM pg_stat_activity`)
+    .then((value) => {
+      return value.rows[0].count;
+    });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: databaseVersion,
+      max_connections: maxConnections,
+      opened_connections: openedConnections,
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
-test("GET to /api/v1/status should return 200", async ()=>{
-	const response = await fetch("http://localhost:3000/api/v1/status")
-	expect(response.status).toBe(200)
-})
+test("GET to /api/v1/status should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  const { database, max_connections, opened_connections } =
+    responseBody.dependencies;
+
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(parseFloat(database)).toBeGreaterThan(0);
+
+  expect(parseInt(max_connections)).toBeGreaterThan(0);
+
+  expect(parseInt(opened_connections)).toBeGreaterThan(0);
+});
