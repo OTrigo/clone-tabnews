11740dc8c9145318203532984a57a5e55beb87bf
Jorge Lucas <37757146+OTrigo@users.noreply.github.com>
2023-12-22 04:39:01 +0000

    feat: finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -9,4 +9,9 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
   
 	expect(responseBody.dependencies.database.version).toEqual("16.0")
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100)
+
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1)
+
 });
