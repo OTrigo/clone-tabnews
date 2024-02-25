c0cbf8f28864e0d7f05f17d4e6feb63a12cbec5e
Jorge Lucas <37757146+OTrigo@users.noreply.github.com>
2024-02-25 01:56:57 +0000

    feat: adding more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -1,5 +1,14 @@
 import { Client } from "pg";
 
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV !== "development";
+}
+
 async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
@@ -7,9 +16,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV !== "development"
+    ssl: getSSLValues(),
   });
-  
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
